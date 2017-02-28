6e0d11aedf82e1d8e7b8640b090565feef7d9530
Julia Kreger <juliaashleykreger@gmail.com>
2017-02-28 20:41:37 +0000

    Correct Release Notes theme
    Change-Id: Ia3bcd96b12810932da5ccee3985c9e953dcee0bd

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -42,6 +42,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'oslosphinx',
     'reno.sphinxext',
 ]
 # Add any paths that contain templates here, relative to this directory.
